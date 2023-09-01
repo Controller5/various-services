756ae682b2ba3727c610807aaef9074d5958832f
Controller5 <128665800+Controller5@users.noreply.github.com>
2023-08-31 23:15:33 -0400

    Update various_1.py

--- various_1.py
+++ various_1.py
@@ -5,12 +5,12 @@ st.set_page_config(
 )
 st.title('Various Services')
 
-center_h = st.number_input('Center of the work should be:', 'step=int')
-height = st.number_input('Height of Work', 'step=int')
-width = st.number_input('Width of Work', 'step=int')   
-from_top = st.number_input('Frame Allowance', 'step=int')
+center_h = st.number_input('Center of the work should be:', step=int)
+height = st.number_input('Height of Work')
+width = st.number_input('Width of Work')   
+from_top = st.number_input('Frame Allowance')
 # from_edge = st.number_input('From Edge')
-space_width = st.number_input('Space Width', 'step=int')
+space_width = st.number_input('Space Width')
 
 center_wall = space_width/2
 screw_height = (center_h + (height/2)) - from_top 
